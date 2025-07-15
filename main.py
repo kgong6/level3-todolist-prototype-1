668a46ee8241ffdf0e382db9353a41035e4552d6
kgong6 <139551411+kgong6@users.noreply.github.com>
2025-07-15 08:55:50 +0000

    Started database entries as form options

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
-from flask import Flask
+import sqlite3
+import random
+
+from flask import Flask, session, render_template, request, g
+
 
 app = Flask(__name__)
+app.secret_key = "iuu78iuytu765kukjngdtrwivukctjn"
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Hello from Flask!'
+    all_items, shopping_items = get_db()
+    return render_template("index.html", all_items = all_items,                shopping_items = shopping_items)
+
+@app.route("/add_items", methods = ["post"])
+def add_items():
+    return request.form["select_items"]
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db=g._database = sqlite3.connect('grocery_list.db')
+        cursor = db.cursor()
+        cursor.execute("select name from groceries")
+        all_data = cursor.fetchall()
+        all_data = [str(val[0]) for val in all_data]
+
+        shopping_list = all_data.copy()
+        random.shuffle(shopping_list)
+        shopping_list = shopping_list[:5]
+    
+    return all_data, shopping_list
+    
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
