ec942feef06ab071e85eea2729b78241fa9c89b5
kgong6 <139551411+kgong6@users.noreply.github.com>
2025-07-21 10:00:44 +0000

    Now can also remove items from the list.

--- main.py
+++ main.py
@@ -24,6 +24,20 @@ def add_items():
                            all_items= session["all_items"], 
                            shopping_items= session["shopping_items"]) 
 
+@app.route("/remove_items", methods = ["post"])
+def remove_items():
+    checked_boxes = request.form.getlist("check")
+
+    for item in checked_boxes:
+        if item in session["shopping_items"]:
+            idx= session["shopping_items"].index(item)
+            session["shopping_items"].pop(idx)
+            session.modified = True
+            
+    return render_template("index.html", 
+           all_items= session["all_items"], 
+           shopping_items= session["shopping_items"])
+
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
