de462ded84f99437ffdc8542636a149a8057a739
kgong6 <139551411+kgong6@users.noreply.github.com>
2025-08-16 10:58:53 +0000

    Needs further debugging. Attempt for the add grocery items part somewhere the user can look for task suggestions.

--- main.py
+++ main.py
@@ -11,47 +11,47 @@ app.config["SESSION_COOKIE_NAME"] = "tfi7865jkhugyutfdt53w4q4ygbctshxro"
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    session["all_items"], session["shopping_items"] = get_db()
+    session["all_items"], session["todo_items"] = get_db()
     return render_template("index.html", 
                            all_items= session["all_items"],
-                           shopping_items= session["shopping_items"])
+                           todo_items= session["todo_items"])
 
 @app.route("/add_items", methods = ["post"])
 def add_items():
-    session["shopping_items"].append(request.form["select_items"])
+    session["todo_items"].append(request.form["select_items"])
     session.modified = True
     return render_template("index.html", 
                            all_items= session["all_items"], 
-                           shopping_items= session["shopping_items"]) 
+                           todo_items= session["todo_items"]) 
 
 @app.route("/remove_items", methods = ["post"])
 def remove_items():
     checked_boxes = request.form.getlist("check")
 
     for item in checked_boxes:
-        if item in session["shopping_items"]:
-            idx= session["shopping_items"].index(item)
-            session["shopping_items"].pop(idx)
+        if item in session["todo_items"]:
+            idx= session["todo_items"].index(item)
+            session["todo_items"].pop(idx)
             session.modified = True
             
     return render_template("index.html", 
            all_items= session["all_items"], 
-           shopping_items= session["shopping_items"])
+           todo_items= session["todo_items"])
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db=g._database = sqlite3.connect('grocery_list.db')
+        db=g._database = sqlite3.connect('todo_list.db')
         cursor = db.cursor()
-        cursor.execute("select name from groceries")
+        cursor.execute("select prompt from suggestions")
         all_data = cursor.fetchall()
         all_data = [str(val[0]) for val in all_data]
 
-        shopping_list = all_data.copy()
-        random.shuffle(shopping_list)
-        shopping_list = shopping_list[:5]
+        todo_list = all_data.copy()
+        random.shuffle(todo_list)
+        todo_list = todo_list[:2]
     
-    return all_data, shopping_list
+    return all_data, todo_list
     
 
 @app.teardown_appcontext
