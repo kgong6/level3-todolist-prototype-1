4f3788f496c3cbd23b7ad6e8e00cbe2c80ec8e18
kgong6 <139551411+kgong6@users.noreply.github.com>
2025-09-09 09:34:44 +0000

    DEBUGGING - now my images changes based on the count (how many checkboxes are ticked) to show progress! and everything works as it should!

--- static/js/main.js
+++ static/js/main.js
@@ -1,42 +1,43 @@
 /* number of times checkbox is ticked off */
-var count = 0;
-var status_image_path = "rainy.gif";
+let count = 0;
 
 /* When task is checked off, strike-through*/
 function check_me(input_id){
-  /*
-this function should put a line through the ticked item
-add one on to a count
-the count should change the hero image
-  */
 
-  //* Hero image changing as tasks get checked off*/
-  function update_Image(count) {
-    if (count >= 3) {
-      status_image_path = 'sunny.gif';
-    } else if (count == 2 ) {
-      status_image_path = 'windy.gif';
-    } else {
-      status_image_path = 'rainy.gif'; 
-    }
-  }
   
   var checked_input = document.querySelector("input[id='" + input_id + "']");
   var checked_label = document.querySelector("label[name='" + input_id + "']");
+  const status_image = document.getElementById('status-image')
   
 
-
+  //* Hero image changing as tasks get checked off*/
+  function update_image(count) {
+    if (count >= 3) {
+        status_image.src = '/static/images/sunny.gif';
+    } else if (count == 2 ) {
+      status_image.src = '/static/images/windy.gif';
+    } else {
+      status_image.src = '/static/images/rainy.gif'; 
+    }
+  }
+  /*
+  this function should put a line through the ticked item
+  add one on to a count
+  the count should change the hero image
+    */
   if (checked_input.checked){
     checked_label.style.textDecoration = "line-through";
     count += 1;
-    console.log(count)
-    update_Image(count)
+    console.log(count);
   }
 
-  else {
-    checked_label.style.textDecoration = "";
+    else {
+      checked_label.style.textDecoration = "";
+      count -=1;
+      console.log(count);
   }
-    
+  
+  update_image(count);
   
 /* Done button also changes colour once checkbox is checked. */
   var btn = document.getElementById("remove_btn");
